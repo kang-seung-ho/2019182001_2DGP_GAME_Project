98d58e4143b70bb5d8c08e2a352e74aca940d866
kangseungho <tyler217@tukorea.ac.kr>
2022-11-08 22:11:34 +0900

    이벤트 예외처리 추가

--- Drill11/boy.py
+++ Drill11/boy.py
@@ -5,8 +5,8 @@ from bullet import bullets
 import title_state
 
 #이벤트 정의
-RD, LD, RU, LU, ATTK, UD, DD, UU, DU = range(9)
-
+RD, LD, RU, LU, ATTK, UD, DD, UU, DU, ATTKU = range(10)
+event_name = ['RD', 'LD', 'RU', 'LU', 'ATTK', 'UD', 'DD', 'UU', 'DU', 'ATTKU' ]
 
 key_event_table = {
     (SDL_KEYDOWN, SDLK_RIGHT): RD, 
@@ -14,16 +14,18 @@ key_event_table = {
     (SDL_KEYUP, SDLK_RIGHT): RU,
     (SDL_KEYUP, SDLK_LEFT): LU,
     (SDL_KEYDOWN, SDLK_v): ATTK,
-    (SDL_KEYDOWN, SDLK_DOWN): UD,
+    (SDL_KEYDOWN, SDLK_UP): UD,
     (SDL_KEYDOWN, SDLK_DOWN): DD,
     (SDL_KEYUP, SDLK_UP): UU,
-    (SDL_KEYUP, SDLK_UP): DU
+    (SDL_KEYUP, SDLK_DOWN): DU,
+    (SDL_KEYUP, SDLK_v): ATTKU
 }
 
 class IDLE:
     def enter(self, event):
         print('ENTER IDLE')
         self.dir = 0
+        self.diry = 0
 
 
     def exit(self, event):
@@ -107,33 +109,33 @@ class ATTACK:
 
     def enter(self, event):
         print('attack')
-
-        if event == RD:
-            self.dir += 1
-        elif event == LD:
-            self.dir -= 1
-        elif event == RU:
-            self.dir -= 1
-        elif event == LU:
-            self.dir += 1
-        elif event == UD:
-            self.diry += 1
-        elif event == UU:
-            self.diry -= 1
-        elif event == DD:
-            self.diry -= 1
-        elif event == DU:
-            self.diry += 1
+        #
+        # if event == RD:
+        #     self.dir += 1
+        # elif event == LD:
+        #     self.dir -= 1
+        # elif event == RU:
+        #     self.dir -= 1
+        # elif event == LU:
+        #     self.dir += 1
+        # elif event == UD:
+        #     self.diry += 1
+        # elif event == UU:
+        #     self.diry -= 1
+        # elif event == DD:
+        #     self.diry -= 1
+        # elif event == DU:
+        #     self.diry += 1
 
     def exit(self, event):
         print('EXIT attack')
-        self.face_dir = self.dir #달리고 있다가 나가게 되더라도 현재 방향을 유지하고 나갈 수 있다.
+        # self.face_dir = self.dir #달리고 있다가 나가게 되더라도 현재 방향을 유지하고 나갈 수 있다.
 
     def do(self):
         self.frame = (self.frame + 1) % 5
 
-        self.x += self.dir
-        self.y += self.diry
+        # self.x += self.dir
+        # self.y += self.diry
 
     def draw(self):
         if self.face_dir == 1:
@@ -145,9 +147,9 @@ class ATTACK:
 
 #상태변환 기술
 next_state = {
-    IDLE: {RU: IDLE, LU: IDLE, RD: RUN, LD: RUN, ATTK: ATTACK, UD: RUN, UU: IDLE, DD: RUN, DU: IDLE},
-    RUN: {RU: IDLE, LU: IDLE, RD:RUN, LD: RUN, ATTK: ATTACK, UD: RUN, UU: IDLE, DD: RUN, DU: IDLE},
-    ATTACK: {RU: IDLE, LU: IDLE, RD: RUN, LD: RUN, ATTK: ATTACK, UD: RUN, UU: IDLE, DD: RUN, DU: IDLE}
+    IDLE:   {RU: RUN, LU: RUN, RD: RUN, LD: RUN, ATTK: ATTACK, UD: RUN, UU: RUN, DD: RUN, DU: RUN, ATTKU: RUN},
+    RUN:    {RU: IDLE, LU: IDLE, RD: IDLE, LD: IDLE, ATTK: ATTACK, UD: IDLE, UU: IDLE, DD: IDLE, DU: IDLE, ATTKU: IDLE},
+    ATTACK: {RU: RUN, LU: RUN, RD: RUN, LD: RUN, ATTK: ATTACK, ATTKU: IDLE, UD: RUN, UU: RUN, DD: RUN, DU: RUN}
 }
 
 
@@ -165,7 +167,7 @@ class Boy:
 
     
     def __init__(self):
-        self.x, self.y = 0, 90
+        self.x, self.y = 1280//2, 720//2
         self.frame = 0
         self.dir = 0
         self.face_dir = 1
@@ -184,7 +186,10 @@ class Boy:
         if self.q:
             event = self.q.pop()
             self.cur_state.exit(self, event) #현재 상태를 나가야 되고.
-            self.cur_state = next_state[self.cur_state][event] #다음 상태를 구한다.
+            try:
+                self.cur_state = next_state[self.cur_state][event] #다음 상태를 구한다.
+            except KeyError:
+                print(self.cur_state, event_name[event])
             self.cur_state.enter(self, event) #다음 상태의 entry action 수행
         
 
